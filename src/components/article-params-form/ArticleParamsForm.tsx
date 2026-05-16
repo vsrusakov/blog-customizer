89021e870f7c08391b7d7117611365bb65548055
vladimir-rusakov <virt.p.q@yandex.ru>
2026-05-16 15:39:26 +0300

    refactor: добавил состояние для формы и реализовал работу сайдбара aside

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,70 @@
+import clsx from 'clsx';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useRef, useState, useEffect, FormEvent } from 'react';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	values: ArticleStateType;
+	onChange: (newValues: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	values,
+	onChange,
+}: ArticleParamsFormProps) => {
+	const [formState, setFormState] = useState(values);
+	const [isOpen, setIsOpen] = useState(false);
+	const containerRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleOutsideClick = (event: MouseEvent) => {
+			if (
+				containerRef.current &&
+				!containerRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClick);
+
+		return () => document.removeEventListener('mousedown', handleOutsideClick);
+	}, [isOpen]);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onChange(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onChange(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen((prev) => !prev)} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}
+				ref={containerRef}>
+				<form className={styles.form} onSubmit={handleSubmit}>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
